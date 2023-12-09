5f9559a94d5d13505027f36eaf877621d724da3d
DSummers2001 <dillon.summers2001@gmail.com>
2023-12-09 07:29:22 -0800

    fixed some typos

--- Player.java
+++ Player.java
@@ -2,26 +2,26 @@ import java.util.Random;
 
 public class Player {
     private Random random = new Random();
-    private String[] temperments = {"Angry", "Funny", "Neutral"};
+    private String[] temperaments = {"Angry", "Funny", "Neutral", "Melancholy"};
     String name;
-    String temperment;
+    String temperament;
 
     public Player(String name){
-        int index = random.nextInt(temperments.length); 
-        this.temperment = temperments[index];
+        int index = random.nextInt(temperaments.length); 
+        this.temperament = temperaments[index];
         this.name = name;
     }
 
     public void selected(){
-        if(this.temperment == "Angry"){
+        if(this.temperament == "Angry"){
             System.out.println("Player: " + name + " was selected.");
             System.out.println("******** Son of a *********");
         }
-        else if(this.temperment == "Funny"){
+        else if(this.temperament == "Funny"){
             System.out.println("Player: " + name + " was selected.");
             System.out.println("Hahahahahah This was fun.");
         }
-        else if(this.temperment == "Neutral"){
+        else if(this.temperament == "Neutral"){
             System.out.println("Player: " + name + " was selected.");
             System.out.println("This emotion does not exist. I am neutral");
         }
